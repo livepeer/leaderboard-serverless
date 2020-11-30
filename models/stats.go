964794e502e6dbe10d94327ef7b872dcdd081743
Nico Vergauwen <vergauwennico@gmail.com>
2020-12-01 00:48:00 +0100

    api,models: aggregated_stats returns normalised scores

--- models/stats.go
+++ models/stats.go
@@ -14,9 +14,9 @@ var Regions = []string{"MDW", "FRA", "SIN"}
 // AggregatedStats are the aggregated stats for an orchestrator
 type AggregatedStats struct {
 	ID             string  `json:"-" bson:"_id,omitempty"`
-	Score          float64 `bson:"score" json:"score"`
 	SuccessRate    float64 `bson:"success_rate" json:"success_rate"`
 	RoundTripScore float64 `bson:"round_trip_score" json:"round_trip_score"`
+	TotalScore     float64 `bson:"score" json:"score"`
 }
 
 // Stats are the raw stats per test stream
